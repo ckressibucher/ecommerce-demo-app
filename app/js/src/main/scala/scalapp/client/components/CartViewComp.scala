87dc707e9b71c4e45460e0a9f2aed5e82d584550
Claudio Kressibucher <ckressibucher@graphicworks.ch>
2016-09-15 15:50:14 +0200

    [WIP] cart view: add qty update, and remove buttons
    Something is broken now with the minicart (maybe also the cart's model)

--- app/js/src/main/scala/scalapp/client/components/CartViewComp.scala
+++ app/js/src/main/scala/scalapp/client/components/CartViewComp.scala
@@ -4,26 +4,77 @@ import diode.data.Pot
 import diode.react.ModelProxy
 import diode.react.ReactPot._
 import japgolly.scalajs.react.extra.router.RouterCtl
-import japgolly.scalajs.react.vdom.ReactNodeFrag
-import japgolly.scalajs.react.{Callback, ReactComponentB}
+import japgolly.scalajs.react.{BackendScope, Callback, ReactComponentB, ReactEventAliases}
 import japgolly.scalajs.react.vdom.prefix_<^._
+import org.scalajs.dom.ext.KeyCode
 
-import scalapp.client.{DiodeDispatcher, Loc, UpdateCartView}
-import scalapp.model.{Price, CartView => CartViewModel}
+import scala.util.Try
+import scalapp.client._
+import scalapp.model.{Price, Product, CartView => CartViewModel}
 
-object CartViewComp {
+object CartViewComp extends ReactEventAliases {
 
   case class Props(proxy: ModelProxy[Pot[CartViewModel]], dispatcher: DiodeDispatcher)
 
   case class MiniCartProps(proxy: ModelProxy[Pot[CartViewModel]], router: RouterCtl[Loc])
 
-  val Line = ReactComponentB[CartViewModel.Line]("cart-line")
-    .render_P(line =>
-      <.tr(
-        <.td(line.p.name.name),
-        <.td(line.qty),
-        <.td(line.taxClass),
-        <.td(^.className := "price-col", PriceBox.PriceBox(line.price))))
+  type Qty = Int
+
+  case class QtyProps(qty: Qty, product: Product, dp: DiodeDispatcher)
+
+  class QtyBackend($: BackendScope[QtyProps, Qty]) {
+
+    def handleChange(e: ReactEventI): Callback = {
+      Try(e.target.value.toInt).map { v: Int =>
+        $.setState(v)
+      }.toOption.getOrElse(Callback.empty)
+    }
+
+    def fire: Callback = (for {
+      props <- $.props
+      qty <- $.state
+    } yield props.dp(UpdateProductQty(props.product, qty))).flatten
+
+    def handleKeyPress(e: ReactKeyboardEvent): Callback = {
+      if (e.keyCode == KeyCode.Enter) fire
+      else Callback.empty
+    }
+
+    def render(s: Qty) =
+      <.div(
+        <.input(
+          ^.className := "qty",
+          ^.`type` := "number",
+          ^.value := s,
+          ^.onChange ==> handleChange,
+          ^.onKeyPress ==> handleKeyPress
+        ),
+        <.button(
+          ^.onClick --> fire,
+          "Update"
+        )
+      )
+  }
+
+  val QtyComponent = ReactComponentB[QtyProps]("cart-line-qty")
+    .initialState_P(props => props.qty)
+    .renderBackend[QtyBackend]
+    .build
+
+  val Line = ReactComponentB[(CartViewModel.Line, DiodeDispatcher)]("cart-line")
+    .render_P {
+      case (line, dp) =>
+        <.tr(
+          <.td(line.p.name.name),
+          <.td(QtyComponent(QtyProps(line.qty, line.p, dp))),
+          <.td(line.taxClass),
+          <.td(^.className := "price-col", PriceBox.PriceBox(line.price)),
+          <.td(
+            <.button(
+              ^.className := "remove",
+              ^.onClick --> dp(RemoveProduct(line.p)),
+              "X")))
+    }
     .build
 
   val TotalsLine = ReactComponentB[CartViewModel.TaxLine]("cart-totals-line")
@@ -48,7 +99,7 @@ object CartViewComp {
           ),
           <.tfoot(
             <.tr(
-            <.td("Total: "), <.td(^.colSpan := 2, PriceBox.PriceBox(cartView.grandTotal))
+              <.td("Total: "), <.td(^.colSpan := 2, PriceBox.PriceBox(cartView.grandTotal))
             )
           )
         )
@@ -69,10 +120,11 @@ object CartViewComp {
                 <.th("Product"),
                 <.th("Quantity"),
                 <.th("Tax class"),
-                <.th(^.className := "price-col", "Line sum"))),
+                <.th(^.className := "price-col", "Line sum"),
+                <.th(^.className := "button-col"))),
             <.tbody(
               cartView.lines.zipWithIndex.map {
-                case (ln: CartViewModel.Line, idx: Int) => Line.withKey(idx)(ln)
+                case (ln: CartViewModel.Line, idx: Int) => Line.withKey(idx)((ln, p.dispatcher))
               })),
           CartTotals(cartView))
       }
